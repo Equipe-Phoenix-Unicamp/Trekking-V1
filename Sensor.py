014e237c00869bbb5a4be85433c3e0c0d8fc5998
otavionettozani <otavionettozani@gmail.com>
2014-07-01 19:56:07 -0300

    sensor Class added

---
+++ Sensor.py
@@ -0,0 +1,48 @@
+import threading
+import time
+class SensorTimer:
+    def defaultAcquisition(self):
+        return 0
+    
+    def __init__(self, acquisitionFrequency, callback, acquisitionFunction=defaultAcquisition):
+        self.acquire = acquisitionFunction
+        self.period = 1/acquisitionFrequency
+        self.callback = callback
+
+
+    def start(self):
+       self.callback(self.acquire())
+       self.threadControl = threading.Timer(self.period, self.start)
+       self.threadControl.start()
+
+    def stop(self):
+        self.threadControl.cancel();
+
+
+class SensorThread(threading.Thread):
+    
+    def defaultAcquisition(self):
+        return 0
+    
+    def __init__(self, acquisitionFrequency, acquisitionFunction=defaultAcquisition):
+        threading.Thread.__init__(self)
+        self.acquire = acquisitionFunction
+        self.period = 1/acquisitionFrequency
+        self.readValue = 0;
+        self.lock = threading.Lock()
+
+    def run(self):
+        while True:
+            time.sleep(self.period)
+            self.lock.acquire()
+            self.readValue = self.acquire()
+            self.lock.release()
+
+    def stop(self):
+        self._stop.set()
+
+    def read(self):
+        self.lock.acquire()
+        retValue = self.readValue
+        self.lock.release()
+        return retValue
